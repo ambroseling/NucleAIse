4c319722870e4abf8988557b461199a28619749b
Ruoyi <ruoyi.xie@mail.utoronto.ca>
2023-11-05 16:26:31 -0500

    Updated encoding_sequence with OS+sequences encoding

--- preprocessing/encoding_sequence.py
+++ preprocessing/encoding_sequence.py
@@ -5,21 +5,27 @@ from transformers import BertTokenizer, BertModel
 # Load CSV data
 data_path = "sample_data.csv"
 df = pd.read_csv(data_path)
+# df['sequence'] = df['OS'] + "," + df['sequence']
 df['sequence'] = df['sequence'].apply(lambda x: ' '.join(list(x))) #add space in between each character in sequence so that BERT model can recognize them and generate different encodings
+df['sequence'] = df['OS'] + df['sequence']
 sequences = df["sequence"].tolist()
-
+# print(sequences[0])
 unique_sequences = list(set(sequences))
 
 # Load tokenizer and model
-model_name = "Rostlab/prot_bert_bfd"
+model_name = "bert-base-cased"
 tokenizer = BertTokenizer.from_pretrained(model_name)
 model = BertModel.from_pretrained(model_name)
 # # Tokenize sequences
-inputs = tokenizer.encode_plus(sequences, return_tensors="pt", padding=True, truncation=True, max_length=512)
+for sequence in sequences:  # Adjust to process more or fewer sequences
+    encoded_sequence = tokenizer.encode_plus(sequence,
+                                             add_special_tokens=True,
+                                             padding=False,
+                                             truncation=False,
+                                             max_length=5005,
+                                             return_tensors="pt")
+    # Extract the input IDs from the encoded sequence
+    input_ids = encoded_sequence['input_ids']
 
-# for seq in sequences:
-#     inputs = tokenizer.encode_plus(seq, return_tensors="pt", truncation=True)
-#     print(inputs['input_ids'])
-
-
-
+    # Print the input IDs
+    print(input_ids)
