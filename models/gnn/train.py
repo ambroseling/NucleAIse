6a917e3d9915d9866be06bcd83f9b798708e552a
RyanS07 <56851349+RyanS07@users.noreply.github.com>
2023-11-25 00:06:32 -0500

    completed implementation of DataLoader for GNN train and test set (still missing ProtBERT embeddings)

--- models/gnn/train.py
+++ models/gnn/train.py
@@ -1,10 +1,11 @@
 import torch
-from torch_geometric.loader.dataloader import DataLoader
+from torch_geometric.loader import DataLoader
 from torch_geometric.data import InMemoryDataset, Data
 from torch_geometric.utils import to_edge_index
 from Bio.PDB import *
 import numpy as np
 import os
+import json
 from csv import DictReader
 import shutil
 import dotenv
@@ -15,7 +16,9 @@ dotenv.load_dotenv()
 class GNNDataset(InMemoryDataset):
     def __init__(self, root, train=True, transform=None, pre_transform=None, pre_filter=None, limit=None):
         self.limit = limit
+        self.train = train
         super().__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
     def raw_file_names(self):
@@ -23,7 +26,7 @@ class GNNDataset(InMemoryDataset):
 
     @property
     def processed_file_names(self):
-        return [os.environ.get("train_set_file"), os.environ.get("test_set_file")]
+        return [os.environ.get("dataset_file")]
 
     def download(self):
         # If all files in raw_file_names are not present in self.raw_dir, get them
@@ -38,17 +41,18 @@ class GNNDataset(InMemoryDataset):
 
     def process(self):
         with open(os.path.join(self.raw_dir, os.environ.get("dataset_csv"))) as dataset_csv:
-            dataset = list(DictReader(dataset_csv))
+            csv_dict = list(DictReader(dataset_csv))
             
         parser = PDBParser()
-        datalist = []
+        dataset = []
         missing_pdbs = []
         alphafold_url_template = os.environ.get('alphafold_url_template')
         count = 0
-        for protein in tqdm(dataset):
+        for protein in tqdm(csv_dict):
             if self.limit is not None and count > self.limit:
                 break
             accession_id = protein["ID"]
+            goa = protein["goa"].strip('][').split(', ')
             alphafold_url = alphafold_url_template.format(accession_id=accession_id)
             alphafold_response = requests.get(alphafold_url)
 
@@ -71,30 +75,41 @@ class GNNDataset(InMemoryDataset):
                             contact_map[i][j] = 0
                         contact_map[j][i] = contact_map[i][j]
                         
-                node_features = []
+                node_embeddings = []
+                embedding_size = 1
                 for i in range(residue_count):
                     # TODO: Apply ProtBERT Embeddings
+                    node_embeddings.append([i])
                     pass
+                node_features = torch.tensor(node_embeddings).view(residue_count, embedding_size)
                 edge_index, edge_attr = to_edge_index(torch.tensor(contact_map).to_sparse())
-                datalist.append(Data(x=node_features, edge_index=edge_index, edge_attr=edge_attr))
+                data = Data(x=node_features, edge_index=edge_index, edge_attr=edge_attr, y=goa, )
+                data.validate(raise_on_error=True)
+                dataset.append(data)
             else:
                 missing_pdbs.append(accession_id)
             
             count += 1
 
-        train_size = int(0.8 * len(datalist))
-        test_size = len(datalist) - train_size
-        train_dataset, test_dataset = torch.utils.data.random_split(datalist, [train_size, test_size])
-        torch.save(train_dataset, self.processed_paths[0])
-        torch.save(test_dataset, self.processed_paths[1])
-
+        torch.save(self.collate(dataset), self.processed_paths[0])
+        
         with open(os.path.join(self.raw_dir, "missing_pdbs.txt"), "w") as missing_pdb_file:
             for pdb in missing_pdbs:
                 missing_pdb_file.write(pdb + "\n")
 
 
-dataset = GNNDataset(os.getcwd() + "/models/gnn", limit=10)
-loader = DataLoader(dataset=dataset, batch_size=64, shuffle=True)
+dataset = GNNDataset(os.getcwd() + "/models/gnn")
+dataset.print_summary()
+partition = int(len(dataset)*0.8)
+train_set = dataset[0:partition]
+test_set = dataset[partition:]
+train_set.print_summary()
+test_set.print_summary()
+train_loader = DataLoader(dataset=train_set, batch_size=64, shuffle=True)
+test_loader = DataLoader(dataset=test_set, batch_size=64, shuffle=True)
 
+# TODO: Start training loop
+for batch in train_loader:
+    pass
 
 
