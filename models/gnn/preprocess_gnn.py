79b837afb5f4a4b29136d4e5c8d59a039cc1a176
RyanS07 <56851349+RyanS07@users.noreply.github.com>
2023-12-08 09:09:15 -0500

    Adjusting gnn preprocessing to save to multiple files

--- models/gnn/preprocess_gnn.py
+++ models/gnn/preprocess_gnn.py
@@ -16,9 +16,10 @@ from tqdm import tqdm
 dotenv.load_dotenv()
 
 class GNNDataset(InMemoryDataset):
-    def __init__(self, root, goa_percentage=1, train=True, transform=None, pre_transform=None, pre_filter=None, limit=None):
+    def __init__(self, root, batch_size, goa_percentage=1, train=True, transform=None, pre_transform=None, pre_filter=None, limit=None):
         self.limit = limit
         self.train = train
+        self.batch_size = batch_size
         self.goa_percentage = goa_percentage
         self.goa_list = []
         with open(os.environ['residue_name_mapping_file'], "r") as f:
@@ -28,9 +29,55 @@ class GNNDataset(InMemoryDataset):
         self.bert_tokenizer = BertTokenizer.from_pretrained(os.environ.get('bert_model_name'))
         self.bert_model = BertModel.from_pretrained(os.environ.get('bert_model_name')).to(self.device)
         super().__init__(root, transform, pre_transform, pre_filter)
-        print(self.processed_paths[0])
-        with open(self.processed_paths[0], "r") as f:
-            self.data = json.load(f)
+        print("Loading Dataset Tensors...")
+        if os.path.exists(self.processed_paths[0]):
+            data_obj = []
+            slices_list = []
+            for file in os.listdir(self.processed_dir):
+                if file[0:13] == 'dataset_batch':
+                    data, slices = torch.load(os.path.join(self.processed_dir, file))
+                    data_obj.append(data)
+                    slices_list.append(slices)
+            self.data, self.slices = self.collate(data_obj)
+            # Generating New Slices
+            assert self.slices['x'].shape[0] == self.slices['edge_index'].shape[0] == self.slices['edge_attr'].shape[0] == self.slices['y'].shape[0]
+            shape = self.slices['x'].shape[0]
+            new_x_slices = slices_list[0]['x']
+            new_edge_index_slices = slices_list[0]['edge_index']
+            new_edge_attr_slices = slices_list[0]['edge_attr']
+            new_y_slices = slices_list[0]['y']
+            for i in range(1, shape-1):
+                # Updating ith x slices
+                updated_x_slices = self.update_slices(slices_list[i]['x'], self.slices['x'][i])
+                new_x_slices = torch.cat((new_x_slices, updated_x_slices))
+
+                # Updating ith edge_index slices
+                updated_edge_index_slices = self.update_slices(slices_list[i]['edge_index'], self.slices['edge_index'][i])
+                new_edge_index_slices = torch.cat((new_edge_index_slices, updated_edge_index_slices))
+
+                # Update ith edge_attr slices
+                updated_edge_attr_slices = self.update_slices(slices_list[i]['edge_attr'], self.slices['edge_attr'][i])
+                new_edge_attr_slices = torch.cat((new_edge_attr_slices, updated_edge_attr_slices))
+
+                # Update ith y slices
+                updated_y_slices = self.update_slices(slices_list[i]['y'], self.slices['y'][i])
+                new_y_slices = torch.cat((new_y_slices, updated_y_slices))
+                
+            self.slices['x'] = new_x_slices
+            self.slices['edge_index'] = new_edge_index_slices
+            self.slices['edge_attr'] = new_edge_attr_slices
+            self.slices['y'] = new_y_slices
+            print(self.data)
+            print(self.slices)
+        else:
+            self.data = None
+            self.slices = None
+
+    def update_slices(self, slices, offset):
+        for j in range(slices.shape[0]):
+            slices[j] += offset
+        slices = slices[1:]
+        return slices
 
     @property
     def raw_file_names(self):
@@ -38,7 +85,7 @@ class GNNDataset(InMemoryDataset):
 
     @property
     def processed_file_names(self):
-        return [os.environ.get("dataset_file")]
+        return [os.environ.get("dataset_complete")]
 
     def download(self):
         # If all files in raw_file_names are not present in self.raw_dir, get them
@@ -87,24 +134,26 @@ class GNNDataset(InMemoryDataset):
             if goa_freq[goa] >= 3:
                 self.goa_list.append(goa)
         self.goa_list.sort()
-        print(self.goa_list)
         goa_map = {}
         for i, goa in enumerate(self.goa_list):
             if i > self.goa_percentage*len(self.goa_list):
                 break
             goa_map[goa] = i
-        print(goa_map)
 
         # Forming Data Loop
         parser = PDBParser()
         dataset = []
+        # full_dataset = []
         missing_pdbs = []
         alphafold_sequence_mismatches = []
         alphafold_url_template = os.environ.get('alphafold_url_template')
         count = 0
-        for protein in csv_dict:
+        batch_num = 1
+        print("Starting Batch " + str(batch_num))
+        for k, protein in enumerate(csv_dict):
             if self.limit is not None and count > self.limit:
                 break
+            print("Protein " + str(k+1))
             accession_id = protein["ID"]
             goa = protein["goa"].strip('][').split(', ')
             output_labels = []
@@ -146,23 +195,34 @@ class GNNDataset(InMemoryDataset):
                         
                 CLS, node_features = self.get_bert_embedding(sequence)
                 edge_index, edge_attr = to_edge_index(torch.tensor(contact_map).to_sparse())
-                data = Data(x=node_features, edge_index=edge_index, edge_attr=edge_attr, y=output_labels)
+                y = torch.tensor(output_labels)
+                data = Data(x=node_features, edge_index=edge_index, edge_attr=edge_attr, y=y)
                 data.validate(raise_on_error=True)
                 dataset.append(data)
+                # full_dataset.append(data)
 
-                if len(dataset) %= 2000:
-                    percent_complete = len(dataset)/200
-                    print(str(percent_complete) + " completed")                 
+                if len(dataset) == self.batch_size:
+                    print(dataset)
+                    path = os.path.join(self.processed_dir, os.environ.get("dataset_file_format").format(i=batch_num))
+                    torch.save(self.collate(dataset), path)
+                    print("Batch " + str(batch_num) + " Completed")
+                    batch_num += 1       
+                    if self.limit is not None and (k+1) < self.limit:
+                        print("\nStarting Batch " + str(batch_num))  
+                    dataset.clear()        
 
             else:
+                print(" - Missing in AlphaFold")
                 missing_pdbs.append(accession_id)
             
             count += 1
-
-        print("Saving Dataset Now")
-        torch.save(self.collate(dataset), self.processed_paths[0])
-        print("Finished Saving Dataset!!!")
         
+        if len(dataset) > 0:
+            print(dataset)
+            path = os.path.join(self.processed_dir, os.environ.get("dataset_file_format").format(i=batch_num))
+            torch.save(self.collate(dataset), path)
+            print("Batch " + str(batch_num) + " Completed")
+
         with open(os.path.join(self.processed_dir, "missing_pdbs.txt"), "w") as missing_pdb_file:
             for pdb in missing_pdbs:
                 missing_pdb_file.write(pdb + "\n")
@@ -171,14 +231,20 @@ class GNNDataset(InMemoryDataset):
             for accessed_id in alphafold_sequence_mismatches:
                 alphafold_mismatch_file.write(accession_id + "\n")
 
-def load_gnn_data():
-    dataset = GNNDataset(os.getcwd() + "/models/gnn", goa_percentage=0.8)
+        with open(os.path.join(self.processed_dir, os.environ.get("dataset_complete")), "w") as completed_flag:
+            completed_flag.write("Completed Processing!")
+
+        # torch.save(full_dataset, os.path.join(self.processed_dir, "full_dataset.pt"))
+
+def load_gnn_data(batch_size, goa_percentage=1, limit=None):
+    dataset = GNNDataset(os.getcwd() + "/models/gnn", batch_size=batch_size, goa_percentage=goa_percentage, limit=limit)
     dataset.print_summary()
 
     train_set = []
     val_set = []
     test_set = []
     dataset_list = list(dataset)
+    print("Creating Train, Val, and Test Sets")
     for goa in dataset.goa_list:
         for protein in dataset_list:
             if goa in set(protein.y):
@@ -205,16 +271,39 @@ def load_gnn_data():
     val_set += dataset_list[train_partition: val_partition]
     test_set += dataset_list[val_partition:]
 
-
-    train_set.print_summary()
-    val_set.print_summary()
-    test_set.print_summary()
-    train_loader = DataLoader(dataset=train_set, batch_size=64, shuffle=True)
-    val_loader = DataLoader(dataset=val_set, batch_size=64, shuffle=True)
-    test_loader = DataLoader(dataset=test_set, batch_size=64, shuffle=True)
+    print("Creating DataLoaders")
+    train_loader = DataLoader(dataset=train_set, batch_size=8, shuffle=True)
+    val_loader = DataLoader(dataset=val_set, batch_size=8, shuffle=True)
+    test_loader = DataLoader(dataset=test_set, batch_size=8, shuffle=True)
     return train_loader,val_loader,test_loader
 
 
 if __name__ == '__main__':
-    train_loader, val_loader, test_loader = load_gnn_data()
+    batch_size = 100
+    train_loader, val_loader, test_loader = load_gnn_data(batch_size=batch_size)
+    reconstructed_dataset = []
+    for batch in train_loader:
+        reconstructed_dataset += batch.to_data_list()
+    
+    for batch in val_loader:
+        reconstructed_dataset += batch.to_data_list()
+
+    for batch in test_loader:
+        reconstructed_dataset += batch.to_data_list()
+
+    # print(reconstructed_dataset)
+    print(len(reconstructed_dataset))
+
+    # original_dataset = torch.load("models/gnn/processed/full_dataset.pt")
+
+    # count = 0
+    # for obj1 in original_dataset:
+    #     # Compare Node Features
+    #     for obj2 in reconstructed_dataset:
+    #         if torch.equal(obj1.x, obj2.x) and torch.equal(obj1.edge_index, obj2.edge_index) and torch.equal(obj1.edge_attr, obj2.edge_attr) and torch.equal(obj1.y, obj2.y):
+    #             count += 1
+    #             break
+
+    # assert count == len(original_dataset)
+    print("DONE!")
 
