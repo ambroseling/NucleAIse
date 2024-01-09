9ace65a05c2c69c01e4bdfef744e1d6a89722b80
Ambrose Ling <lingambrose@gmail.com>
2024-01-08 21:00:05 -0500

    added some more code on eigenvalue + gnn model

--- layers/blocks.py
+++ layers/blocks.py
@@ -1,9 +1,14 @@
 import torch
 from torch.nn import Module
 from torch_geometric.nn import GCNConv, GATConv, PairNorm, InstanceNorm
+from dagnn import DAGNN
+
 from torch_geometric.nn import aggr
+from torch_geometric.data import Data
 import torch_geometric.nn.functional as F
 import torch.nn as nn
+from egnn_pytorch import EGNN
+import math
 
 class GNNBlock(Module):
     def __init__(
@@ -34,7 +39,17 @@ class GNNBlock(Module):
                 bias = params['bias'] if 'bias' in params.keys() else False,
                 kwargs=kwargs
             )
-        else:
+        elif type == "EGNN":
+            self.model = EGNN(dim = params['egnn_dim'],edge_dim=params['edge_dim'])
+        elif type == "GVP":
+            pass
+        elif type == "PointNetConv":
+            self.model = PointNetConv()
+        elif type == "DAGNN":
+            self.model = DAGNN(num_vocab=0,max_seq_len=0,w_edge_attr=False,emb_dim=700,hidden_dim=700,out_dim=700,num_rels=1,num_layers=2,
+                            bidirectional=True,mapper_bias=True,agg_x=True,agg = "attn_h",out_wx=True,out_pool_all=True,out_pool ="max",encoder=None,dropout=0.0,word_vectors=None,emb_dims=[],activation=None,num_class=0,recurr=1)
+
+        elif type == "GCN":
             self.model = GCNConv(
                 in_channels=in_channels, 
                 out_channels=out_channels,
@@ -88,27 +103,48 @@ class GNNBlock(Module):
     def forward(self, data):
         x = data.x
         edge_index = data.edge_index
-        edge_weights = data.edge_weights
+        edge_attr = data.edge_attr
 
         # Pass x into model
-        x = self.model(x, edge_index, edge_weights)
+        print("NODES:")
+        print(x.shape)
+        print("EDGE INDEX:")
+        print(edge_index.shape)
+        print("EDGE ATTR:")
+        print(edge_attr.shape)
+        x = self.model(x = x, edge_index=edge_index)
 
         # Normalize
         if self.norm is not None:
             x = self.norm(x)
 
         if self.residual == "normal":
-           x = self.activation(x) + x
+            x = self.activation(x) + x
         elif self.residual == 'drive':
             x = self.activation(x)*self.drive+x
         else:
-           x = x
+            x = x
             
         data.x = x
         return data
 
-class AttentionBlock(Module):
-    def __init__(self,num_heads,d_model,query_dim,key_dim,value_dim):
+
+# class CrossAttentionBlock(Module):
+#     def __init__(self,num_heads,d_model,query_dim,key_dim,value_dim):
+#         super().__init__()
+#         # self.norm = nn.GroupNorm()
+#         self.num_heads = num_heads
+#         self.scale = None
+#         self.query_proj = nn.Linear(d_model,key_dim*self.num_heads)
+#         self.key_proj = nn.Linear(d_model,key_dim*self.num_heads)
+#         self.value_proj = nn.Linear(d_model,value_dim*self.num_heads)
+#         self.out_proj = nn.Linear(value_dim*self.num_heads,d_model)
+#     def forward(self):
+#         pass
+
+class MultiHeadAttentionBlock(Module):
+    def __init__(self,num_heads,d_model,query_dim,key_dim,value_dim,cross_attn,dropout):
+        super().__init__()
         # self.norm = nn.GroupNorm()
         self.num_heads = num_heads
         self.scale = None
@@ -116,34 +152,114 @@ class AttentionBlock(Module):
         self.key_proj = nn.Linear(d_model,key_dim*self.num_heads)
         self.value_proj = nn.Linear(d_model,value_dim*self.num_heads)
         self.out_proj = nn.Linear(value_dim*self.num_heads,d_model)
+        self.cross_attn = cross_attn
+        if drouput:
+            self.dropout_p = 0.5
+        else:
+            self.dropout_p = 0
 
-    def forward(self,x):
+    def forward(self,data_x,data_y):
         #input shape should be B,L,D
-        # B,L,D = x.shape
-        self.scale = 1/math.sqrt(D)
+        if len(data_x.x.shape) < 3:
+            x = data_x.x.unsqueeze(0)
+        else:
+            x = data_x.x
+        if len(data_y.x.shape) < 3:
+            y = data_y.x.unsqueeze(0)
+        else:
+            y = data_y.x
+        B_x,L_x,D_x = x.shape
+        B_y,L_y,D_y = y.shape
+
+
+        self.scale = 1/math.sqrt(D_x)
         # d_model = D // self.n_heads
         # qkv = torch.repeat_interleave(x,3,dim=1)
         # Q,K,V = torch.chunk(qkv,3,dim=1) #Q,K,V is shape (B,L,D)
-        Q = self.query_proj(x) #Q is shape (B,L,H*Kd')
-        K = self.key_proj(x) #K is shape (B,L,H*Kd')
-        V = self.out_proj(x)#V is shape (B,L,H*Vd')
-        d_query = Q.shape[-1] // self.n_heads
-        d_key = K.shape[-1] // self.n_heads
-        Q = (Q * scale).view(B*self.num_heads,d_query,L)
-        Q = torch.transpose(Q,1,2)
-        K = (K*scale)*view(B*self.num_heads,d_key,L)
+        if self.cross_attn:
+            Q = self.query_proj(x) #Q is shape (B,L,H*Kd')
+            K = self.key_proj(y) #K is shape (B,L,H*Kd')
+            V = self.value_proj(y)#V is shape (B,L,H*Vd')
+            d_query = Q.shape[-1] // self.num_heads
+            d_key = K.shape[-1] // self.num_heads
+            d_value = V.shape[-1] //self.num_heads
+            Q = (Q * self.scale).view(B_x*self.num_heads,d_query,L_x)
+            Q = torch.transpose(Q,1,2)
+            K = (K * self.scale).view(B_y*self.num_heads,d_key,L_y)
+        else:
+            Q = self.query_proj(x) #Q is shape (B,L,H*Kd')
+            K = self.key_proj(x) #K is shape (B,L,H*Kd')
+            V = self.value_proj(x)#V is shape (B,L,H*Vd')
+            d_query = Q.shape[-1] // self.num_heads
+            d_key = K.shape[-1] // self.num_heads
+            d_value = V.shape[-1] //self.num_heads
+            Q = (Q * self.scale).view(B_x*self.num_heads,d_query,L_x)
+            Q = torch.transpose(Q,1,2)
+            K = (K * self.scale).view(B_x*self.num_heads,d_key,L_x)
 
-        weight = torch.matmul(q,k)
+        weight = torch.matmul(Q,K)
+        print("Weight shape: ")
+        print(weight.shape)
         weight = torch.softmax(weight.float(),dim=-1).type(weight.dtype)
-        V = V.reshape(B*self.n_heads,channel,length)
-        weight = torch.transpose(weight,1,2)
+
+        V = V.reshape(B_x*self.num_heads,d_value,L_y)
+        print(V.shape)
+        V = torch.transpose(V,1,2)
         a = torch.matmul(weight,V)
+        a = a.reshape(B_x,L_x,D_x)
+        print("A shape:")
+        print(a.shape)
         out = self.out_proj(a)
-        return out
+        data_x.x = out
+        return data_x
 
-class GNN():
+class FeedForwardBlock(Module):
+    # Data object: 1- edge index, 2- node feature matrix, 3- edge features
+    # 2- shape: Number of nodes x Residue node feature dimension
+    # option 1: 1 (CLS) x 1024 ==> GO node feature dimension x num_go_labels
+    # option 2: seq_len x 1024 ==> GO node feature dimension x num_go_labels
+
+
+    #Input: Seq_len x 1024 ==> Seq_len x go_labels x go_label_dim
+    def __init__(self,num_go_labels,aggr):
+        self.num_go_labels = num_go_labels
+        self.go_dim = go_dim
+        self.fc_units = fc_units
+        self.num_layser = num_layers
+        self.aggr = aggr
+        if activation == "relu":
+            self.activation = nn.ReLU()
+        elif activation == "elu":
+            self.activation = nn.ELU()
+        elif activation == "silu":
+            self.activation = nn.SiLU()
+        if aggr_type == "mean":
+            self.aggr = aggr.MeanAggregation()
+        elif aggr_type == "max":
+            self.aggr = aggr.MedianAggregation()
+        self.layers = []
+        # self.norm = nn.BatchNorm()
+        for i in range(len(self.fc_units)-1):
+            self.layers.append(nn.Linear(self.fc_units[i],self.fc_units[i+1]))
+
+    def forward(self,data):
+        x = data.x
+        for layer in self.layers:
+            x = self.activation(data.x)
+        x = x.view(-1,num_go_labels,go_dim)
+        x = self.aggr(x,index=data.batch)
+        x = x.view(num_go_labels,go_dim)
+        data.x = x
+
+        return data
+        # num_go x go_dim
+
+
+
+
+class GNN(Module):
     def __init__(self,params,**kwargs):
-
+        super().__init__()
         self.num_blocks = params['num_blocks']
         self.norm = params['norm']
         self.channels = params['channels']
@@ -155,14 +271,18 @@ class GNN():
         elif params['fc_act']=='elu':
             self.fc_act = nn.ELU()
         
-
-        self.gnn_blocks = [GNNBlock(self.channels[i], self.channels[i+1],params=params,**kwargs) for i in range(self.num_blocks)]
-        #self.attn_blocks = []
+        self.classifier = params['classifier']
+        self.blocks = []
+        self.attention_heads = params['attention_heads']
+        for i in range(self.num_blocks-1):
+            self.blocks.append(GNNBlock(self.channels[i], self.channels[i+1],params=params,**kwargs))
+            self.blocks.append(nn.LayerNorm(self.channels[i+1]))
+            self.blocks.append(MultiHeadAttentionBlock(self.attention_heads, self.channels[i+1], self.channels[i+1]//self.attention_heads, self.channels[i+1]//self.attention_heads, self.channels[i+1]//self.attention_heads,False,0.5))
+            self.blocks.append(nn.LayerNorm(self.channels[i+1]))
+        
         self.fc = [nn.Linear(self.fc_units[i],self.fc_units[i+1]) for i in range(len(self.fc_units)-1)]
 
-    #     self.go_gnn = DAGNN(num_vocab=0,max_seq_len=0,w_edge_attr=False,emb_dim=700,hidden_dim=700,out_dim=700,num_rels=1,num_layers=2,
-    # bidirectional=True,mapper_bias=True,agg_x=False,agg = "attn_h",out_wx=True,out_pool_all=False,out_pool ="max",encoder=None,dropout=0.0,word_vectors=None,emb_dims=[],activation=None,num_class=0,recurr=1)
-       
+   
         if params['aggr_type'] == "mean":
             self.aggr = aggr.MeanAggregation()
         elif params['aggr_type'] == "max":
@@ -171,12 +291,73 @@ class GNN():
             self.aggr = aggr.SumAggregation()
 
     def forward(self,data):
-        for block in self.gnn_blocks:
-            data = block(data)
+        print(self.blocks)
+        for i in range(len(self.blocks)):
+            data = self.blocks[i](data)
         x = data.x 
-        x = self.aggr(x,index=data.batch)
-        for i,fc in enumerate(self.fc):
-            x = fc(x)
-            if i != len(self.fc)-1:
-                x = self.fc_act(x)
-        return x
+        if self.classifier == True:
+            x = self.aggr(x,index=data.batch)
+            for i,fc in enumerate(self.fc):
+                x = fc(x)
+                if i != len(self.fc)-1:
+                    x = self.fc_act(x)
+            return x
+        else:
+            return x
+
+    
+if __name__ == "__main__":
+    model_params = {
+    'num_classes':500,
+    'channels':[10,1248,2024,1680,2048],
+    'fc_units':[1024,800,500],
+    'egnn_dim':1024,
+    'fc_act':"relu",
+    'heads':4,
+    'concat':True,
+    'negative_slope':0.2,
+    'dropout_p':0,
+    'add_self_loops':False,
+    'edge_dim':None,
+    'fill_value':'mean',
+    'bias':False,
+    'improved':False,
+    'cached':False,
+    'bias':False,
+    'type':'GAT',
+    'aggr_type':'mean',
+    'gnn_act':'relu',
+    'num_blocks':4,
+    'residual_type':'Drive',
+    'attention_heads':4,
+    'classifier':False,
+    'norm':{
+    'norm_type':'PairNorm',
+    'norm_scale':1.0,
+    'norm_scale_individually':False,
+    'norm_eps':1e-05,
+    'norm_momentum:':0.1,
+    'norm_affine':True,
+    'norm_track_running_stats':True
+    }}
+    model = GNN(model_params,type="GCN",activation="relu")
+
+    data_obj_list = []
+    H_x = torch.rand((6,1024)) # 3 nodes, in channels 10
+    edge_index = torch.tensor([[0,1,0],[1,2,2]])# edge index
+    edge_weights = torch.rand((3,5))
+    data_x = Data(x = H_x,edge_index = edge_index,edge_attr = edge_weights)
+    data_obj_list.append(data_x)
+    H_y = torch.rand((10,1024)) # 3 nodes, in channels 10
+    edge_index = torch.tensor([[0,0,0,2,2,2,1],[5,1,2,1,4,3,6]])# edge index
+    edge_weights = torch.rand((7,5))
+    data_y = Data(x = H_y,edge_index = edge_index,edge_attr = edge_weights)
+    data_obj_list.append(data_y)
+    attention = MultiHeadAttentionBlock(4, 1024, 256, 256, 256)
+    output = attention(data_x,data_y)
+    
+    # out = model(data)
+    # print(out)
+
+          
+
---
+++ models/gnn/graph_utils.py
@@ -0,0 +1,143 @@
+import torch
+import scipy
+from scipy.stats import wasserstein_distance
+import matplotlib.pyplot as plt
+
+import sklearn
+import numpy 
+import networkx
+import torch
+from torch_geometric.loader import DataLoader
+from torch_geometric.data import InMemoryDataset, Data
+from torch_geometric.utils import to_edge_index
+from transformers import BertModel, BertTokenizer
+from Bio.PDB import *
+import numpy as np
+import os
+import json
+from csv import DictReader
+import shutil
+import dotenv
+import requests
+import re
+from tqdm import tqdm
+def get_propagation_matrix(A,gnn_type):
+    if gnn_type == "gcn":
+        degrees = torch.sum(A,dim=1)
+        D = torch.sqrt(torch.inverse(torch.diag(degrees)))
+        T = (torch.matmul(torch.matmul(D,A),D))
+    elif gnn_type == "gat":
+        pass
+    return T
+
+def get_laplacian(A=None):
+    D = torch.diag(torch.sum(torch.tensor(A),dim=1))
+    L = D-A
+    return L
+
+def get_cycle_basis(T):
+    pass
+
+def get_eigenvalues(T):
+    w,v = torch.linalg.eig(T)
+    return w,v
+
+def filter_not_in_alphafold(csv_dict):
+    new_csv_dict = []
+    for protein in csv_dict:
+        accession_id = protein["ID"]
+        alphafold_url = alphafold_url_template.format(accession_id=accession_id)
+        alphafold_response = requests.get(alphafold_url)
+        if alphafold_response.status_code == 200:
+            new_csv_dict.append(protein)
+    return new_csv_dict
+
+def retreieve_cmap(protein):
+    accession_id = protein["ID"]
+    alphafold_url = alphafold_url_template.format(accession_id=accession_id)
+    alphafold_response = requests.get(alphafold_url)
+    protein_length = 0
+    if alphafold_response.status_code == 200:
+        pdb_data = requests.get(alphafold_response.json()[0]["pdbUrl"], allow_redirects=True).content
+        pdb_file_name = "/Users/ambroseling/Desktop/NucleAIse/NucleAIse/models/gnn/test.pdb"
+        with open(pdb_file_name, "wb") as pdb_file:
+            pdb_file.write(pdb_data)
+
+        structure = parser.get_structure(accession_id, pdb_file_name)
+        ca_coord = [atom.get_coord() for residue in structure.get_residues() for atom in residue.get_atoms() if atom.get_name() == 'CA']
+        residue_count = len(ca_coord)   
+        protein_length = residue_count                  
+        contact_map = np.zeros((residue_count, residue_count))
+        for i in range(residue_count):
+            for j in range(i, residue_count):
+                distance = np.linalg.norm(ca_coord[i]-ca_coord[j])
+                if distance > 6:
+                    contact_map[i][j] = 1
+                else:
+                    contact_map[i][j] = 0
+                contact_map[j][i] = contact_map[i][j]
+        cmap_th  = torch.tensor(contact_map)
+        print("CMAP CONSTRUCTED SUCCESSFULLY")
+        print(cmap_th.shape)
+        return cmap_th
+    else:
+        print("CANT FIND PROTEIN IN ALPHAFOLD DB")
+        return torch.zeros((protein_length, protein_length))
+
+
+
+# if __name__ == "__main__":
+#     dataset_path = '/Users/ambroseling/Desktop/NucleAIse/NucleAIse/models/gnn/raw/sp_db.csv'
+#     with open(dataset_path) as dataset_csv:
+#         csv_dict = list(DictReader(dataset_csv))
+#     prot_count = 0
+#     parser = PDBParser()
+#     alphafold_url_template = 'https://alphafold.ebi.ac.uk/api/prediction/{accession_id}?key=AIzaSyCeurAJz7ZGjPQUtEaerUkBZ3TaBkXrY94'
+#     eigenvalues_dist = []
+#     print("HOW MANY PROTEINS: ")
+#     print(len(csv_dict))
+#     csv_dict = sorted(csv_dict, key=lambda x: len(x['sequence']))[::100]
+#     csv_dict = filter_not_in_alphafold(csv_dict)
+#     cmaps = []
+#     for protein in csv_dict:
+#         cmaps.append(retreieve_cmap(protein))
+#     for i,protein_a in enumerate(csv_dict):
+#         # print("LENGTH OF PROTEIN: ")
+#         # print(len(protein_a['sequence']))
+#         cmap_a = cmaps[i]
+#         print(f"Processed protein {prot_count}")
+#         prot_count+=1
+#         T_a = get_propagation_matrix(A =cmap_a,gnn_type="gcn")
+#         eigenvalues_a,eigenvectors_a = get_eigenvalues(T_a)
+#         real_eigenvalues_a = torch.view_as_real(eigenvalues_a)
+#         new_eigenvalues_a = torch.sqrt(real_eigenvalues_a[:,0]**2+real_eigenvalues_a[:,1]**2)
+#         eigenval = []
+#         inner_prot_count = 0
+#         for j,protein_b in enumerate(csv_dict):
+#             cmap_b = cmaps[j]
+#             print(f"Processed protein pair {inner_prot_count}")
+
+#             T_b = get_propagation_matrix(A =cmap_b,gnn_type="gcn")
+#             eigenvalues_b,eigenvectors_b = get_eigenvalues(T_b)
+#             real_eigenvalues_b = torch.view_as_real(eigenvalues_b)
+#             new_eigenvalues_b = torch.sqrt(real_eigenvalues_b[:,0]**2+real_eigenvalues_b[:,1]**2)
+#             distance = wasserstein_distance(new_eigenvalues_a.clone().detach().numpy(), new_eigenvalues_b.clone().detach().numpy())
+
+#             eigenval.append(distance)
+#             inner_prot_count+=1
+#         eigenvalues_dist.append(eigenval)
+#     dist = torch.tensor(eigenvalues_dist)
+#     torch.save(dist,"eigenvalue_dist.pt")
+#     print(dist.shape)        
+        
+
+
+
+if __name__ == "__main__":
+    dist = torch.load("/Users/ambroseling/Desktop/NucleAIse/NucleAIse/eigenvalue_dist.pt")
+    plt.imshow(dist.detach().numpy(), cmap='hot', interpolation='nearest')
+    plt.show()
+
+
+
+
